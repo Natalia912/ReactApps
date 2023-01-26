dbc6b768895cc47bc3672bb26b333e67e11a6833
Natalia Tretiakova <tretiakowa2212@gmail.com>
2023-01-27 01:04:32 +0300

    intro component update

--- shortie/src/components/Intro/Intro.tsx
+++ shortie/src/components/Intro/Intro.tsx
@@ -1,7 +1,26 @@
+import Button from '../../ui/Button/Button';
 import './intro.css';
 
 function Intro() {
-  return <section>Intro</section>;
+  return (
+    <section className="intro container">
+      <div className="intro--content">
+        <h1 className="intro--header">More than just shorter links</h1>
+        <p className="intro--description">
+          Build your brand's recognition and get detailed insights on how your
+          links are performing.
+        </p>
+        <Button class="rounded">Get Started</Button>
+      </div>
+      <div className="intro--hero">
+        <img
+          className="intro--heroPic"
+          src="/src/assets/illustration-working.svg"
+          alt="person working in an office"
+        />
+      </div>
+    </section>
+  );
 }
 
 export default Intro;
--- shortie/src/ui/Button/Button.tsx
+++ shortie/src/ui/Button/Button.tsx
@@ -1,7 +1,14 @@
+import { Children, ReactNode } from 'react';
+
 import './button.css';
 
-function Button() {
-  return <button>Button</button>;
+interface ButtonProps {
+  class: 'rounded' | 'square';
+  children: ReactNode;
 }
+
+const Button = (props: ButtonProps) => {
+  return <button className={`button ${props.class}`}>{props.children}</button>;
+};
 
 export default Button;
