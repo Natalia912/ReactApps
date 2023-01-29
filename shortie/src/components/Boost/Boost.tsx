62ed1e5b01ab69ac63d41d8d59659ad59d4fc658
Natalia Tretiakova <tretiakowa2212@gmail.com>
2023-01-29 23:10:00 +0300

    footer and boost components

--- shortie/src/components/Boost/Boost.tsx
+++ shortie/src/components/Boost/Boost.tsx
@@ -1,7 +1,15 @@
+import Button from '../../ui/Button/Button';
 import './boost.css';
 
 function Boost() {
-  return <section>Boost</section>;
+  return (
+    <section className="boost">
+      <div className="container boost-container">
+        <h2 className="boost--header">Boost your links today</h2>
+        <Button class="rounded">Get Started</Button>
+      </div>
+    </section>
+  );
 }
 
 export default Boost;
--- shortie/src/components/Footer/Footer.tsx
+++ shortie/src/components/Footer/Footer.tsx
@@ -1,7 +1,53 @@
+import { ReactComponent as Twitter } from '../../assets/icon-twitter.svg';
+import { ReactComponent as Facebook } from '../../assets/icon-facebook.svg';
+import { ReactComponent as Pinterest } from '../../assets/icon-pinterest.svg';
+import { ReactComponent as Instagram } from '../../assets/icon-instagram.svg';
+
 import './footer.css';
 
 function Footer() {
-  return <footer>Footer</footer>;
+  const columnOne = [
+    'Features',
+    'Link Shortening',
+    'Branded Links',
+    'Analytics'
+  ];
+  const columnTwo = ['Resources', 'Blog', 'Developers', 'Support'];
+  const columnThree = ['Company', 'About', 'Our Team', 'Careers', 'Contact'];
+
+  const footerColumn = (column: string[]) => {
+    return column.map((item, index) => (
+      <li className={index === 0 ? 'column-header' : 'column-item'} key={item}>
+        {item}
+      </li>
+    ));
+  };
+  return (
+    <footer className="footer">
+      <div className="container footer-container">
+        <div className="column column-1">
+          <h2>Shortie</h2>
+        </div>
+        <ul className="column-text column-2">{footerColumn(columnOne)}</ul>
+        <ul className="column-text column-3">{footerColumn(columnTwo)}</ul>
+        <ul className="column-text column-4">{footerColumn(columnThree)}</ul>
+        <ul className="column column-5">
+          <li className="column-social">
+            <Facebook />
+          </li>
+          <li className="column-social">
+            <Twitter />
+          </li>
+          <li className="column-social">
+            <Pinterest />
+          </li>
+          <li className="column-social">
+            <Instagram />
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
