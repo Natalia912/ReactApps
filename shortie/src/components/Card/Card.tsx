2aad14c695e441a021c50122ed36822b87ba78ac
Natalia Tretiakova <tretiakowa2212@gmail.com>
2023-02-05 00:54:28 +0300

    stats component + card

---
+++ shortie/src/components/Card/Card.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react';
+import './card.css';
+
+interface CardI {
+  id: string;
+}
+
+const Card: FC<CardI> = ({ id }) => {
+  const infoArr = [
+    {
+      id: '1',
+      icon: '/src/assets/icon-brand-recognition.svg',
+      title: 'Brand Recognition',
+      description:
+        'Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.'
+    },
+    {
+      id: '2',
+      icon: '/src/assets/icon-detailed-records.svg',
+      title: 'Detailed Records',
+      description:
+        'Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.'
+    },
+    {
+      id: '3',
+      icon: '/src/assets/icon-fully-customizable.svg',
+      title: 'Fully Customizable',
+      description:
+        'Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.'
+    }
+  ];
+
+  const element = infoArr.find((cardInfo) => cardInfo.id === id);
+
+  return (
+    <div className="card">
+      <img src={element?.icon} alt="icon" className="card-icon" />
+      <h4 className="card-title">{element?.title}</h4>
+      <p className="card-description">{element?.description}</p>
+    </div>
+  );
+};
+
+export default Card;
--- shortie/src/components/Stats/Stats.tsx
+++ shortie/src/components/Stats/Stats.tsx
@@ -1,7 +1,19 @@
+import Card from '../Card/Card';
 import './stats.css';
 
 function Stats() {
-  return <section>Stats</section>;
+  return (
+    <section className="stats">
+      <div className="container">
+        <div className="cards-wrapper">
+          <div className="line"></div>
+          <Card id="1" />
+          <Card id="2" />
+          <Card id="3" />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Stats;
