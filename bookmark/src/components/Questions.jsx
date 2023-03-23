c7cbabd3904a74556aaeeec27c9d704a72068851
Natalia Tretiakova <tretiakowa2212@gmail.com>
2023-03-23 22:23:24 +0600

    Update Questions.jsx

--- bookmark/src/components/Questions.jsx
+++ bookmark/src/components/Questions.jsx
@@ -6,7 +6,7 @@ import { questions } from '../content'
 const Questions = () => {
   return (
     <Container>
-      <section className='flex flex-col justify-center w-full lg:mt-32 lg:mb-24 sm:mt-30 my-10 '>
+      <section className='flex flex-col justify-center w-full lg:mt-32 lg:mb-24 sm:mt-30 mt-10 mb-2'>
         <div className='text-center mx-auto'>
           <h2 className={`${styles.sectionHeader} mb-2 sm:mb-6`}>Frequently Asked Questions</h2>
           <p className={`${styles.sectionText} max-w-xl`}>
